8a4c1a19439e3aaad592f48a38c15ec6053e705b
basitol <41623944+basitol@users.noreply.github.com>
2022-03-28 23:22:51 +0100

    Enabled the play and pause

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-// var hello = 12;
+//DOM get element by id
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stops = document.querySelector("#stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
-// // var i = 10;
-// // let j = 12;
+// Event listener for play/pause
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("click", toggleVideoStatus);
 
-// function add() {
-//   //   return i + j;
-//   var i = 15;
-//   var j = 22;
-//   console.log(i + j);
+play.addEventListener("click", toggleVideoStatus);
+stops.addEventListener("click", stopVideo);
+// timestamp.addEventListener("timeupdate", updateProgress);
+// progress.addEventListener("change", updateVideoProgress);
+
+// Play and Pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+// function updatePlayIcon() {
+//   if (video.paused) {
+//     console.log("pause");
+//     play.innerHTML = `<i class="fa fa-play fa-2x"></i>`;
+//   } else {
+//     console.log("play");
+//     play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`;
+//   }
 // }
-// console.log(j);
-// add();
+function updatePlayIcon() {
+  const icon = this.paused ? "fa fa-play fa-2x" : "fa fa-pause fa-2x";
+  play.innerHTML = `<i class="${icon}"></i>`;
+}
