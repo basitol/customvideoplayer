bdd45c8c9add91cfba9af7452a810787fd2ded87
basitol <41623944+basitol@users.noreply.github.com>
2022-03-28 22:19:26 +0100

    Styled the page

---
+++ index.js
@@ -0,0 +1,13 @@
+// var hello = 12;
+
+// // var i = 10;
+// // let j = 12;
+
+// function add() {
+//   //   return i + j;
+//   var i = 15;
+//   var j = 22;
+//   console.log(i + j);
+// }
+// console.log(j);
+// add();
